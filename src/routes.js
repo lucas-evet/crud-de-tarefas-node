a2d3d6a269c9e04f7d40b8572ca2acd61df28f31
lucasevet <lucase.ssilva@hotmail.com>
2026-01-29 09:45:49 -0300

    fix: corrigido o erro de tipo persist do database

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      if (!title || !description) {
+        return res
+          .writeHead(400)
+          .end(
+            JSON.stringify({ message: 'title and description are required' })
+          )
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const tasks = database.select('tasks')
+      return res.end(JSON.stringify(tasks))
+    }
+  }
+]
